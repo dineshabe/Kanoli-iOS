15bf803b08b90800afa331745eb82a8f28b782b5
dineshabe <dineshabe@gmail.com>
2017-03-26 10:31:55 -0600

    Initial commit

---
+++ VideoSkeleton/VideoSkeleton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoSkeleton
+//
+//  Created by Dinesh_Personal on 2017-03-26.
+//  Copyright © 2017 Dinesh_Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
